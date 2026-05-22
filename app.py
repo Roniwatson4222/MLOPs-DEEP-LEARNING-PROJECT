a19a32e5390433ddc9d19cb324079918d7fb0c44
Sanjiv <roniwatson42@gmail.com>
2026-05-23 00:56:36 +0530

    Correcting the port in app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp('InputImage.jpg')
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
