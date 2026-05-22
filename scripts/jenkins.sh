10772a7ad06749058896c6330a1f1a2077a473e9
Sanjiv <roniwatson42@gmail.com>
2026-05-22 23:20:45 +0530

    updating jenkinsfile

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -34,6 +34,11 @@ sudo usermod -a -G docker jenkins
 
 
 ## AWS configuration & restarts jenkins
+sudo apt install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 
 aws configure
 
